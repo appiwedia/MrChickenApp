8cdf664eabc2212e65a641937d64906b0e0af1c6
Mickael MAS <mickael.mas@appiwedia.com>
2020-07-10 20:01:44 +0200

    Initial Commit

---
+++ MrChickenApp/MrChickenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MrChickenAppApp.swift
+//  MrChickenApp
+//
+//  Created by Mickael Mas on 10/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct MrChickenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
